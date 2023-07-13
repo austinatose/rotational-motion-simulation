5080311bf96979c2845f54462603d36d6a15fa49
NeeNawAmbulance <51728263+NeeNawAmbulance@users.noreply.github.com>
2023-07-13 21:22:15 +0800

    big progress on slider values

--- car.js
+++ car.js
@@ -2,14 +2,16 @@ class Car {
     // rectangle placeholder for now, will replace with image of car
     constructor(xpos, ypos) {
         this.pos = createVector(xpos, ypos);
-        this.angvel = 2; // radians per second
+        this.angvel = 1; // radians per second
         this.angle = 0; // radians, from rotcentre
-        this.velocity = 0;
-        this.mass = 10;
+        this.velocity = 17.25;
+        this.mass = 1500;
         this.sizemult = 1; // changes with mass
         this.rotcentre = createVector(450, 350); // pivot point (centre of track)
-        this.radius = 100; // distance from rotcentre
+        this.radius = 200; // distance from rotcentre
+        this.trueradius = (this.radius - 100) * 1.8 / 100 + 5.4; // actual radius of track (m)
         this.cforce = this.mass * this.radius * this.angvel^2; // centripetal force
+        // car is 1.8 m wide, size multiplier to turn pixels to metres
     }
 
     display() {
@@ -17,7 +19,7 @@ class Car {
         translate(this.pos.x, this.pos.y);
         rotate(this.angle);
         rectMode(CENTER);
-        rect(0, 0, 50, 100);
+        rect(0, 0, 25, 50);
         pop();
     }
 
@@ -25,6 +27,16 @@ class Car {
         this.angle += -this.angvel * deltaTime / 1000;
         this.pos.x = this.rotcentre.x + cos(this.angle) * this.radius;
         this.pos.y = this.rotcentre.y + sin(this.angle) * this.radius;
-        this.cforce = this.mass * this.radius * this.angvel^2;
+        this.trueradius = map(this.radius, 100, 300, 4, 19); //
+        this.velocity = this.angvel * this.trueradius;
+        this.cforce = this.mass * this.trueradius * this.angvel * this.angvel;
+    }
+
+    updateAngVel() {
+        this.angvel = this.velocity / this.trueradius;
+    }
+
+    updateVel() {
+        this.velocity = this.angvel * this.trueradius;
     }
 }
--- sketch.js
+++ sketch.js
@@ -13,36 +13,57 @@ function draw() {
   drawTrack();
   drawSidebarText();
   car1.display();
+  
 
   // check for value update
-  if (radSlider.value() != car1.radius) {
-    car1.radius = radSlider.value();
-  } else if (massSlider.value() != car1.mass) {
-    car1.mass = massSlider.value();
-  } else if (angvelSlider.value() != car1.velocity) {
-    car1.angvel = angvelSlider.value();
-  }
+  car1.radius = radSlider.value();
+  car1.mass = massSlider.value();
 
-  car1.update();
+  velSlider.input(updateCarAngVel);
+  angvelSlider.input(updateCarVel);
+  radSlider.input(updateCarVel);
+
   
+  car1.update();
 }
 
 function drawSidebarSliders() {
   radSlider = createSlider(100, 300, 200, 10);
-  radSlider.position(1000, 200);
-  angvelSlider = createSlider(0, 10, 2, 0.1);
+  radSlider.position(1000, 100);
+  velSlider = createSlider(0, 28.5, 17.25, 0.5);
+  velSlider.position(1000, 200);
+  angvelSlider = createSlider(0, 1.5, 2, 0.1);
   angvelSlider.position(1000, 300);
-  massSlider = createSlider(50, 300, 200, 10);
+  massSlider = createSlider(1000, 2500, 1500, 10);
   massSlider.position(1000, 400);
 }
 
 function drawSidebarText() {
-  text("F_Centripetal = " + car1.cforce, 1000, 500);
+  
+  text("Angular Velocity = " + round(car1.angvel, 1), 1000, 350);
+  text("Mass = " + car1.mass, 1000, 450);
+  text("F_Centripetal = " + round(car1.cforce), 1000, 500);
+  text("Radius = " + round(car1.trueradius, 1), 1000, 150);
+  text("Raw Radius = " + car1.radius, 1000, 650);
+  text("Velocity = " + round(car1.velocity, 1), 1000, 250);
 }
 
 function drawTrack() {
-  noFill();
-  circle(450, 350, 100);
+  fill(63, 63, 71);
   circle(450, 350, 690);
+  circle(450, 350, 100);
+  
   fill(0);
+}
+
+function updateCarVel() {
+  car1.angvel = angvelSlider.value();
+  car1.velocity = car1.angvel * car1.trueradius;
+  velSlider.value(car1.velocity);
+}
+
+function updateCarAngVel() {
+  car1.velocity = velSlider.value();
+  car1.angvel = car1.velocity / car1.trueradius;
+  angvelSlider.value(car1.angvel);
 }
